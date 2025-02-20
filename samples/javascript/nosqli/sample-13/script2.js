7e043c412d240fb7f6e7f3e5aee50a861fbac640
Berg Severens <berg@aikido.dev>
2025-02-20 13:55:29 +0100

    added sample

---
+++ samples/javascript/nosqli/sample-13/script2.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+app.post("/login", function (req, res) {
+    const database = client.db("database");
+    const Users = database.collection("users");
+    const user = Users.findOne({
+        name: req.body.name,
+        password: req.body.password,
+    });
+
+    if (user) {
+        res.send(`Welcome, ${user}`);
+    } else {
+        res.send("User not found");
+    }
+});
