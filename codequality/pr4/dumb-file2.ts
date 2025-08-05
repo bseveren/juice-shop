46378dd5f822dc1e039a503adcbcd98cee17a212
Berg Severens <berg@aikido.dev>
2025-08-05 21:23:28 +0200

    adding commit

--- codequality/pr4/dumb-file2.ts
+++ codequality/pr4/dumb-file2.ts
@@ -2,18 +2,6 @@ export function extract(a: number, b: number): number {
     return a + b;
 }
 
-export function extract2(a: number, b: number): number {
-    return a + b;
-}
-
-export function extract3(a: number, b: number): number {
-    return a + b;
-}
-
-export function extracta(a: number, b: number): number {
-    return a + b;
-}
-
 export function wgern(fer: string, fnerh: number[], wnher: any) {
     let fnhe = `fhrth ${fer}`;
     fnhe += `${fnerh}`;
