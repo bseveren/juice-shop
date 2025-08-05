8b80cf8d2c1e424853ef9855d48d199588186344
Berg Severens <berg@aikido.dev>
2025-08-05 22:02:05 +0200

    adding commit

--- codequality/pr5/dumb-file2.ts
+++ codequality/pr5/dumb-file2.ts
@@ -9,3 +9,7 @@ export function extract2(a: number, b: number): number {
 export function extract3(a: number, b: number): number {
     return a + b;
 }
+
+export function extract4(a: number, b: number): number {
+    return a + b;
+}
