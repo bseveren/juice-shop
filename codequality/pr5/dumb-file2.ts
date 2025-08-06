6221317535229ebd42d5a6fde32bf4e07bf81f0e
Berg Severens <berg@aikido.dev>
2025-08-06 08:45:10 +0200

    dummy commit

--- codequality/pr5/dumb-file2.ts
+++ codequality/pr5/dumb-file2.ts
@@ -9,11 +9,3 @@ export function wefgherg(a: number, b: number): number {
 export function whrthw(a: number, b: number): number {
     return a + b;
 }
-
-export function extract4(a: number, b: number): number {
-    return a + b;
-}
-
-export function extract5(a: number, b: number): number {
-    return a + b;
-}
