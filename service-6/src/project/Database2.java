94cb66a5e38ae4bad8ba72af6fb20cdeb251e5bf
Berg Severens <berg@aikido.dev>
2025-09-16 17:54:40 +0200

    adding commit

--- service-6/src/project/Database2.java
+++ service-6/src/project/Database2.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class Database {
     public static void query(String input) {
-        // some comment 2
+        // some comment 3
         String input = (String) Util.sanitizeSqlInput(args[0]);
         try {
             Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "", "");
