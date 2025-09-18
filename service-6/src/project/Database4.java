5a79a8901662ad233e8d27230e902c8b86ed0722
Berg Severens <berg@aikido.dev>
2025-09-18 09:16:34 +0200

    adding comment

--- service-6/src/project/Database4.java
+++ service-6/src/project/Database4.java
@@ -6,6 +6,7 @@ public class Database {
     public static void query(String input) {
         input = (String) Util.sanitizeSqlInput(input);
         try {
+            // 2nc comment
             Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "", "");
 
             Statement stmt = conn.createStatement();
